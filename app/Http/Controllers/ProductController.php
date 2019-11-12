ae74c6c91e403e441e4c5ee1d17d5f539eb3127d
valera.mishin <valera13021987@gmail.com>
2019-11-12 15:22:56 +0200

    middleware for auth:api added to ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,12 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
+
     public function index()
     {
         return ProductCollection::collection(Product::paginate(5));
